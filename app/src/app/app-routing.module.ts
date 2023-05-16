0c33d00f86e715066da7e068b78e29ec513cc9b7
Jose-cmd98 <jose1234roberto@gmail.com>
2023-05-16 16:47:40 -0300

    feature/first-routing

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeDirectiveComponent } from './home-directive/home-directive/home-directive.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'Directives'
+  },
+  {
+    path: 'Directives',
+    component: HomeDirectiveComponent,
+    loadChildren: () => import ('./home-directive/home-directive.module').then(m => m.HomeDirectiveModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
